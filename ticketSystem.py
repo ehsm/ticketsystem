de539ec743709f9e3cc4913b8c4f6c8c6bc56880
Stefan Laufmann <stefan.laufmann@mailbox.tu-berlin.de>
2012-06-24 21:09:49 +0200

    check commands works too

--- ticketSystem.py
+++ ticketSystem.py
@@ -25,12 +25,12 @@ def randHashString():
 	return randString
 	
 def commandCreate(args):
-	if(len(args.name) > 255):
+	nameParts = args.name.split('=')
+	name = nameParts[len(nameParts)-1]
+	if(len(name) > 255):
 		print "Please choose a name that fits into 255 characters"
 		quit()
 	else:
-		nameParts = args.name.split('=')
-		name = nameParts[len(nameParts)-1]
 		randString = randHashString()
 		userCode = qrtools.QR(data=randString)
 		fname = "tmp/"+randString+".png"
@@ -43,29 +43,33 @@ def commandCreate(args):
 		dbConn.commit()
 		dbConn.close()
 
-def commandCheck(code):
-	dbCursor = mysqlConnect()
-	dbCursor.execute("SELECT name FROM tickets WHERE code="+code+";")
-	rows = cursor.fetchall()
+def commandCheck(args):
+	code = qrtools.QR()
+	code.decode_webcam()
+	dbConn = mysqlConnect()
+	dbCursor = dbConn.cursor()
+#	code.data="9640f4432618567"
+	dbCursor.execute("SELECT name FROM tickets WHERE code=%s;", [code.data])
+	rows = dbCursor.fetchall()
 	if (len(rows) < 1):
 		print "Sorry, there is no ticket registered with this code."
 	elif (len(rows) > 1):
 		print "Something went wrong because there are multiple tickets with this code."
 	else:
-		name = rows[0].name
+		name = rows[0][0]
 		print "Ticket is registered on "+name
-	cursor.close()
+	dbCursor.close()
+	dbConn.close()
 
 def createParser():
 	parser = argparse.ArgumentParser()
 	subparsers = parser.add_subparsers()
-	# create a parser for the create subcommand and add arguments
+	# create a parser for the create subcommand and add arguments and handler function
 	parserCreate = subparsers.add_parser('create')
 	parserCreate.add_argument('name', type=str, help="ticket owners name")
 	parserCreate.set_defaults(func=commandCreate)
-	# create a parser for the check subcommand and add arguments
+	# create a parser for the check subcommand and add handler function 
 	parserCheck	= subparsers.add_parser('check')
-	parserCheck.add_argument('code', type=str, help="identification code of the ticket")
 	parserCheck.set_defaults(func=commandCheck)
 	return parser
 
@@ -76,11 +80,3 @@ if __name__ == "__main__":
 		there is no way to match any rules on peoples names so i'm stopping trying to find one.
 		if (len(sys.argv) < 3 or not re.match('[a-zA-Z]+$', sys.argv[2]) or len(sys.argv[2]) > 32):"""
 	args.func(args)
-"""	if (args):
-		if (len(parserCreate.name) > 255):
-			print "Please choose a name that fits into 255 characters"
-		else:
-			commandCreate(parserCreate.name)
-	elif (parserCheck):
-		commandCheck(parserCheck.code)
-	quit()"""
