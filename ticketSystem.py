7c5c36cfdf0bf47ff3004bdbe025a36b0129ae53
Stefan Laufmann <stefan.laufmann@mailbox.tu-berlin.de>
2012-06-27 00:34:01 +0200

    checks the "used" field in the database, marks checked tickets as "used" and checks wether a code already exists before adding it as a new one

--- ticketSystem.py
+++ ticketSystem.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
+#
+# Copyright (C) 2012 Stefan Laufmann <mail@stefanlaufmann.de>
+# Licensed under GPL v3 or later
 
 import MySQLdb		# communicate with mysql database
 import sys			# access commandline parameters, execute commands
-import re			# regular expressions, obviously
 import argparse		# parse commandline arguments and print help texts
 import qrtools		# encode and decode QR-Codes
 import os			# access urandom for "good" random numbers
@@ -10,6 +12,7 @@ import hashlib		# provides hash functions
 import ConfigParser	# parse config files to set database settings
 
 CONF_FILE = "dbAuth.conf"
+BASE_DIR = "tmp/"
 
 def mysqlConnect(config):
 	hostname = config.get('ticketSystem', 'SERVER_URL')
@@ -22,30 +25,55 @@ def mysqlConnect(config):
 							db=database)
 	return conn
 
-"""TODO: create tickets with LaTeX per python"""
-def createPrintTicket(qrCode):
-	return True	
+"""TODO: create tickets with LaTeX per subcommand
+	Thoughts:	Put it into a seperate class?
+				Read the positions of the fields from a config file as well?"""
+def createPrintTicket(qrCode, name):
+	filepath = os.path.join(BASE_DIR, qrCode.data + ".tmp")
+	file = open(filepath, 'w')
+	file.write(r"""
+\documentclass{article}
+
+\usepackage[papersize={9cm,6cm}, margin=0cm]{geometry}
+\usepackage[utf8]{inputenc}
+\usepackage[ngerman]{babel}
+\usepackage{graphicx}
+\usepackage{overpic}
+% use DejaVuSans as default font
+\usepackage{DejaVuSans}
+\renewcommand*\familydefault{\sfdefault} %% Only if the base font of the document is to be sans serif
+\usepackage[T1]{fontenc}
+
+\begin{document}
+	\centering
+	\begin{overpic}[width=9cm]{ticketEntwurf}
+		\put(5,25){\huge{""" + name + """}}	
+	\end{overpic}
+\end{document}
+""")
 
 def randHashString():
 	randData = os.urandom(128)
-	randString = hashlib.md5(randData).hexdigest()[:15]
+	randString = hashlib.md5(randData).hexdigest()[:16]
 	return randString
 	
 def commandCreate(args, config):
 	nameParts = args.name.split('=')
 	name = nameParts[len(nameParts)-1]
-	if(len(name) > 255):
-		print "Please choose a name that fits into 255 characters"
+	if len(name) > 32:
+		print "Please choose a name that fits into 32 characters"
 		quit()
 	else:
 		randString = randHashString()
-		userCode = qrtools.QR(data=randString)
+		while checkCode(randString, config):
+			randString = randHashString()
 		fname = "tmp/"+randString+".png"
-		userCode.encode(filename=fname)
-		createPrintTicket(userCode.filename)
 		dbConn = mysqlConnect(config)
 		dbCursor = dbConn.cursor()
-		dbCursor.execute("INSERT INTO tickets SET name=%s, code=%s;", [name, randString])
+		dbCursor.execute("INSERT INTO tickets SET name=%s, code=%s, used=0;", [name, randString])
+		userCode = qrtools.QR(data=randString)
+		userCode.encode(filename=fname)
+		createPrintTicket(userCode, name)
 		dbCursor.close()
 		dbConn.commit()
 		dbConn.close()
@@ -53,20 +81,37 @@ def commandCreate(args, config):
 def commandCheck(args, config):
 	code = qrtools.QR()
 	code.decode_webcam()
+	dbData = checkCode(code.data, config)
+	name = dbData[0]
+	used = dbData[1]
+	if not dbData:
+		print "Sorry, there is no ticket registered with this code."
+	elif used:
+		print "Sorry, the ticket registered to " + name + " was already used."
+	else:
+		markAsUsed(code.data, config)
+		print "Ticket is registered on " + name
+		print "Ticket was marked as used."
+
+def markAsUsed(code, config):
 	dbConn = mysqlConnect(config)
 	dbCursor = dbConn.cursor()
-#	code.data="9640f4432618567"
-	dbCursor.execute("SELECT name FROM tickets WHERE code=%s;", [code.data])
+	dbCursor.execute("UPDATE tickets SET used=1 WHERE code=%s;", code)
+	dbCursor.close()
+	dbConn.commit()
+	dbConn.close()
+
+def checkCode(code, config):
+	dbConn = mysqlConnect(config)
+	dbCursor = dbConn.cursor()
+	dbCursor.execute("SELECT name, used FROM tickets WHERE code=%s;", [code])
 	rows = dbCursor.fetchall()
-	if (len(rows) < 1):
-		print "Sorry, there is no ticket registered with this code."
-	elif (len(rows) > 1):
-		print "Something went wrong because there are multiple tickets with this code."
-	else:
-		name = rows[0][0]
-		print "Ticket is registered on "+name
 	dbCursor.close()
 	dbConn.close()
+	if (len(rows) < 1):
+		return None
+	else:
+		return rows[0]
 
 def parseConfig(confFile):
 	config = ConfigParser.SafeConfigParser()
