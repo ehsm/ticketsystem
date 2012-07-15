e314d59da34e226e0e33337c84dd2769c7eb88a9
Merten Sach <merten@tabinin.eu>
2012-07-15 13:56:16 +0200

    For now change to CSV database. Creation of png tickets with barcode. Creation of personal printable PDF tickets from HTML.

---
+++ ticketSettings.py
@@ -0,0 +1,109 @@
+class Ticket():
+    TEMPLATE_FILE="data/ticket_template.png"
+    OUTPUT_DIR="printTickets/"
+    OUTPUT_FORMAT="png"
+
+    NAME_POS=(20,320)
+    NAME_MAX_LENGTH=265
+    BARCODE_POS=(5,380)
+
+    FONT="data/OCRAitalic.otf"
+    FONT_SIZE=10
+    TICKET_NAME_POS=(20, 345)
+    PRICE_POS=(20, 365)
+
+class Database():
+    SERVER_URL="localhost"
+    SERVER_USER="ehsm"
+    SERVER_PASSWORD="exceptional"
+    SERVER_DATABASE="ehsmTickets"
+    NAME_LENGTH=32
+    CODE_LENGTH=12
+
+class TicketPage():
+    OUTPUT_DIR="pageTicket/"
+    TEMPLATE=u"""
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <title>EHSM 2012 / December 28-30 / Berlin</title>
+
+    <link rel="stylesheet" type="text/css" href="data/style.css" />
+
+    </head>
+
+    <body>
+
+    <div id="header">
+    <div id="headerinside">
+    <div id="logo">EHSM</div>
+    </div>
+    </div>
+
+    <div id="container">
+    <div id="title">
+    <p>
+    Exceptionally Hard &amp; Soft Meeting<br />
+    exploring the frontiers of open source and DIY<br />
+    Berlin, December 28-30 2012</p>
+    </div>
+    </div>
+
+    <table>
+    <tr>
+    <td>
+    <img src="%(ticket)s" alt="%(code)s" style="border: 2px solid #000;" />
+    </td>
+    <td>
+    <div id="container">
+    <div id="message">
+    <p>
+    Hello %(name)s,
+    </p>
+    <p>
+    the Exceptionally Hard &amp; Soft Meeting in approaching fast. So we are happy to send you your ticket.
+    </p>
+    <p>
+    Please print your ticket and present it at the cash desk to retrieve your wristband.
+    </p>
+    <p>
+    Below you find additional useful information.
+    </p>
+    <p>
+    Your EHSM Team
+    </p>
+    </div>
+    </div>
+    </td>
+    </tr>
+    </table>
+
+
+    <div id="container">
+    <div id="intro">
+
+    <h2>EHSM venue</h2>
+    <p>
+    TU Berlin, H&ouml;rsaalgeb&auml;ude Elektrotechnik<br />
+    Lecture room HE101<br />
+    Stra&szlig;e des 17. Juni 136<br />
+    10623 Berlin, Germany<br />
+    U Bahn: Ernst-Reuter-Platz
+    </p>
+    </div>
+
+    </div>
+
+    <div id="footer">
+    <div id="footerinside">
+    <p>This is the life on Mars. <a href="http://www.twitter.com/ehsmeeting">Follow us on twitter</a></p>
+    </div>
+    </div>
+
+    </body>
+    </html>
+	"""
--- ticketSystem.py
+++ ticketSystem.py
@@ -1,144 +1,228 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
 # Copyright (C) 2012 Stefan Laufmann <mail@stefanlaufmann.de>
 # Licensed under GPL v3 or later
 
-import MySQLdb		# communicate with mysql database
-import sys			# access commandline parameters, execute commands
-import argparse		# parse commandline arguments and print help texts
-import qrtools		# encode and decode QR-Codes
-import os			# access urandom for "good" random numbers
-import hashlib		# provides hash functions
-import ConfigParser	# parse config files to set database settings
+import MySQLdb        # communicate with mysql database
+import sys            # access commandline parameters, execute commands
+import argparse        # parse commandline arguments and print help texts
+import os            # access urandom for "good" random numbers
+import hashlib        # provides hash functions
+import logging
+import StringIO
+import ho.pisa as pisa
+import csv
+from PIL import Image, ImageDraw, ImageFont
+import barcode
 
-CONF_FILE = "ticketSystem.conf"
-BASE_DIR = "tmp/"
+import ticketSettings as config
 
-def mysqlConnect(config):
-	hostname = config.get('database', 'SERVER_URL')
-	username = config.get('database', 'SERVER_USER')
-	userpw = config.get('database', 'SERVER_PASSWORD')
-	database = config.get('database', 'SERVER_DATABASE')
-	conn = MySQLdb.connect(	host=hostname,
-							user=username,
-							passwd=userpw,
-							db=database)
-	return conn
+def mysqlConnect():
+    conn = MySQLdb.connect(    host=config.Database.SERVER_URL,
+                            user=config.Database.SERVER_USER,
+                            passwd=config.Database.SERVER_PASSWORD,
+                            db=config.Database.SERVER_DATABASE)
+    return conn
 
-"""TODO: create tickets with LaTeX per subcommand
-	Thoughts:	Put it into a seperate class?
-				Read the positions of the fields from a config file as well?"""
-def createPrintTicket(qrCode, name):
-	return True
+def createPrintTicket(config, code, name, ticketName, ticketPrice):
+    canvas = Image.open(config.Ticket.TEMPLATE_FILE)
+
+    #Generate Barcode
+    code39 = barcode.get_barcode_class('code39')
+    barcodeImage = code39(code, writer=barcode.writer.ImageWriter())
+    barcodeImage = barcodeImage.render({'dpi':140, 'text':code,'module_height':10})
+
+    #Insert Barcode
+    canvas.paste(barcodeImage, config.Ticket.BARCODE_POS)
+    drawCanvas = ImageDraw.Draw(canvas)
+    x,y = config.Ticket.BARCODE_POS
+    font = ImageFont.truetype(config.Ticket.FONT,10)
+    drawCanvas.text((x +180 ,y-10), code, (0,0,0),font=font)
+
+    #Render Ticket Name & Price
+    ticketName = ticketName.decode('utf8')
+    ticketPrice = ticketPrice.replace("&euro;","€").decode('utf8')
+    font = ImageFont.truetype(config.Ticket.FONT, config.Ticket.FONT_SIZE)
+    drawCanvas.text(config.Ticket.TICKET_NAME_POS, ticketName, (0,0,0), font=font)
+    drawCanvas.text(config.Ticket.PRICE_POS, ticketPrice, (0,0,0), font=font)
+
+    #Render Name
+    font = ImageFont.truetype(config.Ticket.FONT,20)
+    name = name.decode('utf8')
+    while True:
+        (w, h) = drawCanvas.textsize(name,font=font)
+        if w < config.Ticket.NAME_MAX_LENGTH:
+            break
+        else:
+            name =  name[:-1]
+    drawCanvas.text(config.Ticket.NAME_POS, name, (0,0,0), font=font)
+
+
+    ticketImagePath = "%s/%s.%s" % (config.Ticket.OUTPUT_DIR,
+                                    code,
+                                    config.Ticket.OUTPUT_FORMAT)
+    try:
+        canvas.save(ticketImagePath)
+    except:
+        print "Cannot save ticket image to %s" % (ticketImagePath,)
+
+    pageTicketPath = "%s/%s.pdf" % (config.TicketPage.OUTPUT_DIR, code)
+    htmlTemplate = config.TicketPage.TEMPLATE % { 'name' : name,
+                                                  'code' : code,
+                                                  'ticket' : ticketImagePath,
+                                                  'price' : ticketPrice,
+                                                  'desc' : ticketName }
+    try:
+        pisa.CreatePDF(StringIO.StringIO(htmlTemplate.encode('ascii', 'xmlcharrefreplace')),
+                       open(pageTicketPath,'wb'))
+    except:
+       print "Cannot write PDF ticket to %s" % (pageTicketPath,)
+
+"""
+Printer class to print piso errors to the console.
+"""
+class LogPrinter(logging.Handler):
+    def emit(self, record):
+        print record
+
+def readCSV(filename):
+    return csv.reader(open(filename, 'rb'), delimiter=',', quotechar='"')
+ 
+def writeCSV(filename, truncate=True):
+    if truncate:
+        return csv.writer(open(filename, 'wb'), delimiter=',', quotechar='"')
+    else:
+        return csv.writer(open(filename, 'ab'), delimiter=',', quotechar='"')
+
+def importCSV(args):
+    if not args.input or not args.output:
+        sys.exit(1)
+    inCSV = readCSV(args.input)
+    outCSV = writeCSV(args.output, args.truncate)
+    for line in inCSV:
+        ticketName = line[2]
+        ticketPrice = line[3]
+        email = line[4]
+        name = line[5]
+        code = randHashString(12)
+        outCSV.writerow([code,name,email,ticketName,ticketPrice])
+
+def createTicketFromCSV(args):
+    if not args.file:
+        sys.exit(0)
+    csv = readCSV(args.file)
+    for entry in csv:
+        code, name, email, ticketName, ticketPrice= entry
+        createPrintTicket(config, code, name, ticketName, ticketPrice)
 
 def randHashString(length):
-	randData = os.urandom(128)
-	randString = hashlib.md5(randData).hexdigest()[:length]
-	return randString
-	
-def commandInstall(args, config):
-	config = parseConfig(args.config)
-	dbRootPass = readfromcmdline(passwd)
-	hostname = config.get('database', 'SERVER_URL')
-	mysqlConn = MySQLdb.connect(	host=hostname,
-									user="root",
-									passwd=dbRootPass)
-	dbName = config.get('database', 'SERVER_DATABASE')
-	mysqlConn.execute('CREATE DATABASE %s ;', dbName)
-	mysqlConn.execute('CREATE TABLE ticets ;')
-	dbUser = config.get('datbase', 'SERVER_USER')
-	mysqlConn.execute('CREATE USER  %s ;', sbUser)
-	mysqlConn.execute('GRANT PRIVILEGES TO %s ;', dbUser)
+    randData = os.urandom(128)
+    randString = hashlib.md5(randData).hexdigest()[:length]
+    return randString
+    
+def commandInstall(args):
+    dbRootPass = readfromcmdline(passwd)
+    mysqlConn = MySQLdb.connect(    host=config.Database.SERVER_URL,
+                                    user="root",
+                                    passwd=dbRootPass)
+    mysqlConn.execute('CREATE DATABASE %s ;', config.Database.SERVER_DATABASE)
+    mysqlConn.execute('CREATE TABLE ticets ;')
+    mysqlConn.execute('CREATE USER  %s ;', config.Database.SERVER_USER)
+    mysqlConn.execute('GRANT PRIVILEGES TO %s ;', config.Database.SERVER_USER)
 
 
-def commandCreate(args, config):
-	nameLength = config.getint('database', 'NAME_LENGTH')
-	codeLength = config.getint('database', 'CODE_LENGTH')
-	nameParts = args.name.split('=')
-	name = nameParts[len(nameParts)-1]
-	if (len(name) > nameLength):
-		print "Please choose a name that fits into "+ str(nameLength) +" characters"
-		quit()
-	else:
-		randString = randHashString(codeLength)
-		while checkCode(randString, config):
-			randString = randHashString(codeLength)
-		fname = "tmp/"+randString+".png"
-		dbConn = mysqlConnect(config)
-		dbCursor = dbConn.cursor()
-		dbCursor.execute("INSERT INTO tickets SET name=%s, code=%s, used=0;", [name, randString])
-		userCode = qrtools.QR(data=randString)
-		userCode.encode(filename=fname)
-		createPrintTicket(userCode, name)
-		dbCursor.close()
-		dbConn.commit()
-		dbConn.close()
+def commandCreate(args):
+    nameParts = args.name.split('=')
+    name = nameParts[len(nameParts)-1]
+    if (len(name) > config.Database.NAME_LENGTH):
+        print "Please choose a name that fits into %s characters" % (config.Database.NAME_LENGTH,)
+        quit()
+    else:
+        randString = randHashString(config.Database.CODE_LENGTH)
+        while checkCode(randString):
+            randString = randHashString(config.Database.CODE_LENGTH)
+        fname = "tmp/"+randString+".png"
+        dbConn = mysqlConnect()
+        dbCursor = dbConn.cursor()
+        dbCursor.execute("INSERT INTO tickets SET name=%s, code=%s, used=0;", [name, randString])
+        userCode = qrtools.QR(data=randString)
+        userCode.encode(filename=fname)
+        createPrintTicket(userCode, name)
+        dbCursor.close()
+        dbConn.commit()
+        dbConn.close()
 
-def commandCheck(args, config):
-	if not args.code:
-		code = qrtools.QR()
-		code.decode_webcam()
-		data = code.data
-	else:
-		data = args.code
-	dbData = checkCode(data, config)
-	name = dbData[0]
-	used = dbData[1]
-	if not dbData:
-		print "Sorry, there is no ticket registered with this code."
-	elif used:
-		print "Sorry, the ticket registered to " + name + " was already used."
-	else:
-		markAsUsed(data, config)
-		print "Ticket is registered on " + name
-		print "Ticket was marked as used."
+def commandCheck(args):
+    if not args.code:
+        code = qrtools.QR()
+        code.decode_webcam()
+        data = code.data
+    else:
+        data = args.code
+    dbData = checkCode(data, config)
+    name = dbData[0]
+    used = dbData[1]
+    if not dbData:
+        print "Sorry, there is no ticket registered with this code."
+    elif used:
+        print "Sorry, the ticket registered to " + name + " was already used."
+    else:
+        markAsUsed(data, config)
+        print "Ticket is registered on " + name
+        print "Ticket was marked as used."
 
-def markAsUsed(code, config):
-	dbConn = mysqlConnect(config)
-	dbCursor = dbConn.cursor()
-	dbCursor.execute("UPDATE tickets SET used=1 WHERE code=%s;", code)
-	dbCursor.close()
-	dbConn.commit()
-	dbConn.close()
+def markAsUsed(code):
+    dbConn = mysqlConnect()
+    dbCursor = dbConn.cursor()
+    dbCursor.execute("UPDATE tickets SET used=1 WHERE code=%s;", code)
+    dbCursor.close()
+    dbConn.commit()
+    dbConn.close()
 
-def checkCode(code, config):
-	dbConn = mysqlConnect(config)
-	dbCursor = dbConn.cursor()
-	dbCursor.execute("SELECT name, used FROM tickets WHERE code=%s;", [code])
-	rows = dbCursor.fetchall()
-	dbCursor.close()
-	dbConn.close()
-	if (len(rows) < 1):
-		return None
-	else:
-		return rows[0]
-
-def parseConfig(confFile):
-	config = ConfigParser.SafeConfigParser()
-	config.read(confFile)
-	return config
+def checkCode(code):
+    dbConn = mysqlConnect()
+    dbCursor = dbConn.cursor()
+    dbCursor.execute("SELECT name, used FROM tickets WHERE code=%s;", [code])
+    rows = dbCursor.fetchall()
+    dbCursor.close()
+    dbConn.close()
+    if (len(rows) < 1):
+        return None
+    else:
+        return rows[0]
 
 def createParser():
-	parser = argparse.ArgumentParser()
-	subparsers = parser.add_subparsers()
-	# create a parser for the create subcommand and add arguments and handler function
-	parserCreate = subparsers.add_parser('create')
-	parserCreate.add_argument('name', type=str, help="ticket owners name")
-	parserCreate.set_defaults(func=commandCreate)
-	# create a parser for the check subcommand and add handler function 
-	parserCheck	= subparsers.add_parser('check')
-	parserCheck.add_argument("--code", help="manually give the ticket code to check for")
-	parserCheck.set_defaults(func=commandCheck)
-	# create a parser for an install routine
-	parserInstall = subparsers.add_parser('install')
-	parserInstall.add_argument('config', help="specifiy the config file to use for setting up the database")
-	parserInstall.set_defaults(func=commandInstall)
-	return parser
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    #Create tickets from csv
+    parserCreateCSV = subparsers.add_parser('createCSV')
+    parserCreateCSV.add_argument('-i', dest='file', type=str, help='csv file')
+    parserCreateCSV.set_defaults(func=createTicketFromCSV)
+    # Import data form csv
+    parserImportCSV = subparsers.add_parser('importCSV')
+    parserImportCSV.add_argument('-i', type=str, dest='input', help='csv file')
+    parserImportCSV.add_argument('-o', type=str, dest='output', help='output csv file')
+    parserImportCSV.add_argument('--truncate', action='store_true', default=False, help='truncate file instead of appending')
+    parserImportCSV.set_defaults(func=importCSV)
+    # create a parser for the create subcommand and add arguments and handler function
+    parserCreate = subparsers.add_parser('create')
+    parserCreate.add_argument('name', type=str, help="ticket owners name")
+    parserCreate.set_defaults(func=commandCreate)
+    # create a parser for the check subcommand and add handler function 
+    parserCheck    = subparsers.add_parser('check')
+    parserCheck.add_argument("--code", help="manually give the ticket code to check for")
+    parserCheck.set_defaults(func=commandCheck)
+    # create a parser for an install routine
+    parserInstall = subparsers.add_parser('install')
+    parserInstall.add_argument('config', help="specifiy the config file to use for setting up the database")
+    parserInstall.set_defaults(func=commandInstall)
+    return parser
 
-if __name__ == "__main__":
-	parser = createParser()
-	args = parser.parse_args()
-	"""	As mentioned here: http://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names
-		there is no way to match any rules on peoples names so i'm stopping trying to find one."""
-	config = parseConfig(CONF_FILE)	# parse the config file
-	args.func(args, config)		# execute the right function depending on the arguments
+if __name__ == "__main__": 
+    parser = createParser()
+    args = parser.parse_args()
+    logging.getLogger('ho.pisa').addHandler(LogPrinter())
+    """    As mentioned here: http://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names
+        there is no way to match any rules on peoples names so i'm stopping trying to find one."""
+    args.func(args)        # execute the right function depending on the arguments
